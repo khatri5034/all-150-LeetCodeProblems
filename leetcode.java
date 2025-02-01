121cd997140bcc4984750bb1e81143f59b376dcf
kk <keon.pgm@gmail.com>
2025-01-31 16:54:14 -0800

    added leetCode #49

--- leetcode.java
+++ leetcode.java
@@ -62,3 +62,26 @@ public class leetcode {
     
 }
 }
+// #49. Given an array of strings strs, group the anagrams
+// together. You can return the answer in any order.
+//Example 1:
+//Input: strs = ["eat","tea","tan","ate","nat","bat"]
+//Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+
+        for(String s:strs){
+            char[]chars= s.toCharArray();
+            Arrays.sort(chars);
+            String k =new String(chars);
+            if(!map.containsKey(k)) {
+                map.put(k, new ArrayList<>());
+            }
+            map.get(k).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+
+    }
+}
